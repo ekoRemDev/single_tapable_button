8f13f702f2c29c09293c207c196e0943b53489cb
Ekrem Moralioglu <ekoremdev@gmail.com>
2022-12-01 10:13:48 +0400

    initial setup

--- lib/single_tapable_buttons.dart
+++ lib/single_tapable_buttons.dart
@@ -1,7 +1,77 @@
-library single_tapable_buttons;
+import 'dart:async';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class SingleTapableButton extends StatefulWidget {
+  final void Function(StreamSink<bool> canBePressed) onPressed;
+  final Widget? child;
+  final String? text;
+  final Color backgroundColor;
+  final bool disabled;
+
+  const SingleTapableButton({
+    required this.onPressed,
+    this.child,
+    this.text,
+    this.backgroundColor = Colors.blue,
+    this.disabled = false,
+  });
+
+  @override
+  State<SingleTapableButton> createState() => _SingleTapableButtonState();
+}
+
+class _SingleTapableButtonState extends State<SingleTapableButton> {
+  final StreamController<bool> canButtonBePressedStream = StreamController();
+
+  @override
+  void initState() {
+    super.initState();
+    canButtonBePressedStream.sink.add(!widget.disabled);
+  }
+
+  @override
+  void dispose() {
+    canButtonBePressedStream.close();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(SingleTapableButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.disabled != widget.disabled) {
+      canButtonBePressedStream.sink.add(!widget.disabled);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: StreamBuilder(
+          stream: canButtonBePressedStream.stream,
+          builder: (context, snapshot) {
+            var _canBePressed = (snapshot.data ?? false);
+            return ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                backgroundColor: widget.backgroundColor,
+                elevation: 0,
+              ),
+              onPressed: _canBePressed
+                  ? () {
+                      canButtonBePressedStream.sink.add(false);
+                      widget.onPressed(canButtonBePressedStream.sink);
+                    }
+                  : null,
+              child: widget.text != null
+                  ? Text(
+                      widget.text!,
+                    )
+                  : widget.child ?? const SizedBox(),
+            );
+          }),
+    );
+  }
 }
